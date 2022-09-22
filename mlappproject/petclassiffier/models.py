1eedc82d67fb9eb7183e6d377b78ea354711506c
Miguel Sosa <miguelsosa2003@gmail.com>
2022-09-22 10:43:26 -0500

    Proyecto completo

---
+++ mlappproject/petclassiffier/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class mlModels(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    architecture = models.FileField(upload_to='mimodels/')
+    weights = models.FileField(upload_to='mimodels/')
+    priority = models.PositiveSmallIntegerField(null=True)
